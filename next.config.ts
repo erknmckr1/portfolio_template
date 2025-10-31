e49d54b15a03fca1f8a1df2bfc2bc440459479cb
Erkan Mustafa Çakır <105559804+erknmckr1@users.noreply.github.com>
2025-10-31 16:00:28 +0300

    Remove static export and image optimization settings
    Removed output configuration and image optimization settings.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",  // Next.js siteyi tamamen statik HTML olarak dışa aktarır.
-  images: {
-    unoptimized: true,  // <Image> bileşenleri optimize edilmeden direkt render edilir.
-  },
+
 };
 
 export default nextConfig;
